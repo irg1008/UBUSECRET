be4554bfd4cdb3cbaf46ae2b5e563dc63b11e4d9
irg1008 <ivansudevlop@gmail.com>
2021-09-29 19:51:16 +0200

    First commit on UBUSECRET

---
+++ UBUSECRET/App/Secret.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    public class Secret
+    {
+        private String id;
+        private String sender;
+        private List<String> receiversId;
+        private String message;
+        private String key;
+
+        /* CONSTRUCTORES */
+        public Secret(string sender, List<string> receiversId, string message)
+        {
+            // TODO - generar id
+            this.sender = sender;
+            this.receiversId = receiversId;
+            this.message = message;
+            // TODO - generar clave
+        }
+
+        /* GETTERS AND SETTERS */
+        public string Id { get => id; set => id = value; }
+        public string Sender { get => sender; set => sender = value; }
+        public List<string> ReceiversId { get => receiversId; set => receiversId = value; }
+        public string Message { get => message; set => message = value; }
+        public string Key { get => key; set => key = value; }
+
+        /* FUNCIONES */
+
+        // TODO - Yo lo haria void
+        public bool attachUser(String userId)
+        {
+            receiversId.Add(userId);
+            return true;
+        }
+
+        // TODO
+        public bool dettachUser(String userId)
+        {
+            return receiversId.Remove(userId);
+        }
+
+        // TODO
+        public void createLink(List<User> allowedUsers)
+        {
+
+        }
+
+    }
+}
---
+++ UBUSECRET/App/User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica_01
+{
+    enum Role
+    {
+        ADMIN,
+        USER
+    }
+
+    public class User
+    {
+        private String id;
+        private String name;
+        private String surname;
+        private String email;
+        private String password; // Hashed String
+        private List<String> attachedSecrets;
+        private bool verified; // Cambiamos esto por "estado".
+        private List<Role> roles;
+
+        /* CONSTRUCTOR */
+        public User(String name, String surname, String email, String password)
+        {
+            // TODO - generar id
+            Name = name;
+            Surname = surname;
+            Email = email;
+            attachedSecrets = new List<string>();
+            // TODO - cifrado de password
+        }
+
+        /* GETTERS AND SETTERS */
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public List<string> AttachedSecrets { get => attachedSecrets; set => attachedSecrets = value; }
+        public bool Verified { get => verified; set => verified = value; }
+        internal List<Role> Roles { get => roles; set => roles = value; }
+
+        /* FUNCIONES */
+
+        // TODO
+        public bool checkPasword(String password)
+        {
+            return password == this.password;
+        }
+
+        // TODO - revisar
+        public bool addSecret(Secret secret)
+        {
+            attachedSecrets.Add(secret.Id);
+            return false;
+        }
+
+        // TODO - revisar
+        public bool updateSecret(Secret secret)
+        {
+            if (attachedSecrets.Remove(secret.Id))
+            {
+                attachedSecrets.Add(secret.Id);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        // TODO
+        public bool deleteSecret(String secretId)
+        {
+            return attachedSecrets.Remove(secret.Id);
+        }
+
+        // TODO
+        public bool refreshArrachedSecrets()
+        {
+            return false;
+        }
+
+        // TODO
+        public bool detachFromSecret(String secretId)
+        {
+            return false;
+        }
+
+    }
+
+    
+}
