40b9474b5e0c2590091860b8546e7dad20729f28
ChocoAlmendrado <pss1005@alu.ubu.es>
2021-12-22 19:29:02 +0100

    Interface added

--- UBUSECRET/DataAPI/IDB.cs
+++ UBUSECRET/DataAPI/IDB.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿using Main;
+using System;
+using System.Collections.Generic;
+
 
 namespace DataAPI
 {
-    public class IDB
+    public interface IDB
     {
+        List<User> listUsers();
+
+        List<User> listActiveUsers();
+
+        List<User> listUnactiveUsers();
+
+        bool addSecret(Secret secret);
+
+        bool getSecret(int id);
+
+        Secret removeSecret(int id);
+
+        List<Secret> listSecrets();
+
+        List<Secret> listOwnSecrets(User user);
+
+        List<Secret> listReceivedSecrets(User user);
     }
 }
