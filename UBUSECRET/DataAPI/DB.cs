4d0f5a9751480468f32826cfca74adc5446b26a7
ChocoAlmendrado <pss1005@alu.ubu.es>
2021-12-22 19:32:34 +0100

    Implementacion base db

--- UBUSECRET/DataAPI/DB.cs
+++ UBUSECRET/DataAPI/DB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Main;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,49 @@ namespace DataAPI
 {
     class DB : IDB
     {
+        public bool addSecret(Secret secret)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool getSecret(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> listActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Secret> listOwnSecrets(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Secret> listReceivedSecrets(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Secret> listSecrets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> listUnactiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> listUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Secret removeSecret(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
